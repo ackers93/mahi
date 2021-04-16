145f5aca136585d1546e9f01be47f454f2bf30ba
Andrew Ackerman <andrew.ackers@gmail.com>
2021-04-16 15:59:24 -0700

    Bootstrap, conditional buttons and formatting

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :trainees
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "goals#index"
+  root to: "main#index"
 end
